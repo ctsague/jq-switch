223831c0f1c855585cfddf11d7f1ce499c5e6c26
ctsague <tsagueymele@yahoo.fr>
2016-10-07 14:11:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,35 +5,15 @@ var species = {
 };
 
 function main(){
-
-	/* 
-	tout votre code doit se trouver dans cette fonction,
-	vous pouvez biensur créer d'autres fonctions si nécessaire
-	*/
-
 	// 1. placez un listener sur le click des éléments <button>
 	$(".button").click(function(){
-	// 2. dans le callback récupérez le data attribute animal
+		// 2. dans le callback récupérez le data attribute animal
 		var animalType = $(this).data('animal');
-		//console.log(animalType);
-	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
-		var photo=(species[animalType]);
-		//console.log(photo);
-
-	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
-	
-		$("img").attr('src', 'img/'+photo); //source et chemin
-
-	// 5. inserez cet élément dans l'élément ayant l'id holder
-
-
+		// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
+		var photo=species[animalType];
+		// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
+		$("img").attr('src', 'img/'+photo); //source et chemin de l'image
 	});
-
-	
-	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
-	
-	
-	 
 }
 
 $(document).ready(function(){
